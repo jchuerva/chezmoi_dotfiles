ee3021f1f1c79a05e3e10ca92f3d439e9d808e23
Carlos Alarcon <jchuerva@github.com>
2021-04-23 17:16:31 +0200

    add zsh plugins

--- run_once_intall-zsh.sh
+++ run_once_intall-zsh.sh
@@ -2,3 +2,9 @@
 
 # install ohmyzsh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh-syntax-highlighting
+/bin/bash -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+
+# zsh-autosuggestions
+/bin/bash -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
