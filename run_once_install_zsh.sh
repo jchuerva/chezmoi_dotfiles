283cf3b454ef253367fd19c06dc0e948af1a7ec1
Carlos Alarcon <jchuerva@github.com>
2021-04-27 17:34:27 +0200

    zsh script

---
+++ run_once_install_zsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# installs oh-my-zsh
+echo "\e[32m[DOT]\e[34m installing oh my zsh ... \e[39m\n"
+! sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null 2>&1
+
+echo "\e[32m[DOT]\e[34m installing oh my zsh plugins ... \e[39m\n"
+
+# installs zsh auto suggestions
+! git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" > /dev/null 2>&1
+
+# installs zsh-syntax-highlighting
+! git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" > /dev/null 2>&1
+
